bb6465089182c3f345a90ad300b143626d03f244
halim <abdelhalim.bouguedra@emploitic.com>
2018-09-28 18:52:59 +0100

    break console_log steps into separate functions / add a class wrapper

--- console_log.php
+++ console_log.php
@@ -1,141 +1,210 @@
 <?php
 /**
- * Log a variable number of PHP variables to the js console
+ * Dump PHP variables to the js console
  * @param: (mixed) 
  */
 function console_log()
 {
-    $argv = array();
-    foreach(func_get_args() as $var_key => $value) {
-        $argv['__ARG__' . $var_key] = $value;
-    }
-    $serialized = var_export($argv, true);
-    $formatted = preg_replace('/(\s.*?::__set_state\()/i', '', $serialized);
-    $formatted = preg_replace('/(\)\))/i', ')', $formatted);
-    eval('$args =' . $formatted . ';');
-    $args_json = json_encode($args);
+    $argv = func_get_args();
+    $json = __ConsoleLog__::getFormattedJson($argv);
 
-    echo "<script>
-             var jsonDump = JSON.parse('$args_json'),
-                consoleArgs = [],
-                indentationStr = '  ',
+    // add the js helper script if not already included
+    if (!__ConsoleLog__::$script_included) {
+        echo "<script>" . __ConsoleLog__::getJsPrettyPrintFunction() . "</script>";
+        __ConsoleLog__::$script_included = true;
+    }
+
+    __ConsoleLog__::callJsPrettyPrint($json);
+}
+
+// Wrapper class for console_log
+class __ConsoleLog__
+{
+    public static $script_included = false;
+
+    /**
+     * export a variable to a json string
+     * @param $argv mixed the parameters to format
+     * @return mixed|string
+     */
+    public static function getFormattedJson($argv)
+    {
+
+        if (sizeof($argv) === 1 && is_scalar($argv[0])) {
+            $argv = $argv[0];
+        } else {
+            // Make sure keys are converted to strings to be later formatted in js
+            foreach ($argv as $k => $v) {
+                $argv[gettype($v)] = $v;
+                unset($argv[$k]);
+            }
+
+            // <HACK (lines 40 to 57): expose non public object properties and class names
+            $argv_str = var_export($argv, true);
+
+            // replace objects identifiers by their respective class names
+            $object_identifier = '/((\')?(\S*?)(\')?\s*?=>\s*?(\S*))::__set_state\(/';
+            $argv_str = preg_replace_callback(
+                $object_identifier,
+                function ($matches) {
+                    $key = $matches[3];
+                    $className = '(' . $matches[sizeof($matches) - 1] . ')';
+                    return "'$key:$className' => ";
+                },
+                $argv_str
+            );
+            // Remove extra parentheses
+            $argv_str = preg_replace('/\)\)/', ')', $argv_str);
+
+            // evaluate the result string (reverse var_export)
+            eval('$argv =' . $argv_str . ';');
+            // HACK/>
+        }
+
+        // export to json
+        return json_encode($argv);
+    }
+
+    /**
+     * echo a script tag containing a call to the js formatter function on the provided json
+     * @param $json
+     */
+    public static function callJsPrettyPrint($json)
+    {
+        // Call the js formatter on the json
+        echo ("<script>
+            (function() {
+                 var jsonDump = JSON.parse('$json');
+                 prettyPrintPHPJson(jsonDump);
+            })();
+         </script>");
+    }
+
+    /**
+     * echo the js formatter function
+     */
+    public static function getJsPrettyPrintFunction()
+    {
+        echo "<script>
+           function prettyPrintPHPJson(jsonDump) {
+                var consoleArgs = [],
+                indentationStr = '|  ',
+                defaultColors = [
+                    '#820597',
+                    '#3e49a8',
+                    '#357FC9',
+                    '#2D86AD',
+                    '#35b0bc',
+                    '#a3af67',
+                ],
+                styles = {
+                    property : {
+                        base: 'font-weight: 600;',
+                        colorsByIndentLevel: defaultColors
+                    },
+                    brace : {
+                        base: 'color: #555555; font-weight: 600;',
+                        colorsByIndentLevel: defaultColors
+                    },
+                    bracket : {
+                        base: 'color: #ba576b; font-weight: 400;',
+                        colorsByIndentLevel: defaultColors
+                    },
+                    nullValue : {
+                        base: 'color: #aaa; font-weight: 600;',
+                        colorsByIndentLevel: []
+                    },
+                    pipe: {
+                        base: 'color: #00000022; font-size: 70%;font-weight: 100;',
+                        colorsByIndentLevel: []
+                    }
+                },
                 regexes = {
-                    stylable: /\"[a-zA-Z0-9_]+?\":|\{|\}|\[|\]|null/gm,
+                    stylable: /\".+?\":|\{|\}|\[|\]|null|\|/gm,
                     breakAfter: /null,|\",|[0-9],|\],|\},|\{|\[/gm,
                     breakBefore: /\]|\}/gm,
-                },
-                styles = {
-                   property : {
-                       base: 'font-weight: 600;',
-                       colorsByIndentLevel: [
-                          '#444', 
-                          '#519DD2',
-                          '#3F789E',
-                          '#1BA5D2', 
-                          '#69b6ba', 
-                          '#ace383', 
-                          '#D2A283'
-                       ]
-                   },
-                   brace : {
-                       base: 'color: #D79F49; font-weight: 600;',
-                       colorsByIndentLevel: []
-                   },
-                   bracket : {
-                       base: 'color: #ba576b; font-weight: 600;',
-                       colorsByIndentLevel: []
-                   },
-                   nullValue : {
-                       base: 'color: #e98500; font-weight: 600;',
-                       colorsByIndentLevel: []
-                   },
+                    argsKey: /(\s{2})\"([0-9]*)\"( →)/gm
                 };
-             
-             function getStyle(type, indentationLevel) {
-                 var baseStyle = styles[type].base;
-                 var computedStyle = '';
-                 var color = styles[type].colorsByIndentLevel[indentationLevel - 1];
-                 if (color) {
-                     computedStyle += 'color:' + color + ';'
-                 }
-                 return baseStyle + computedStyle;
-             }
-            
-             function addStyles(match, offset, str, consoleArgs){
+        
+            function getStyle(type, indentLevel) {
+                var baseStyle = styles[type].base;
+                var computedStyle = '';
+                var color = styles[type].colorsByIndentLevel[indentLevel - 1];
+                if (color) {
+                    computedStyle += 'color:' + color + ';'
+                }
+                return baseStyle + computedStyle;
+            }
+        
+            function addStyles(match, offset, str, consoleArgs){
                 var replacement = '%c%s%c';
                 var lastChar = match[match.length - 1];
-                var indentationLevel = getIndetationLevel(str.substr(0, offset));
-                var closingIndentation = indentationLevel ? indentationLevel - 1 : 0;
+                var indentLevel = getIndetationLevel(str.substr(0, offset));
+                var closingIndentation = indentLevel ? indentLevel - 1 : 0;
                 switch (lastChar) {
                     case ':':
-                        replacement = '%c%s%c ';
-                        style = getStyle('property', indentationLevel);
+                        var argKey =  /\"(.*?)\":/i;
+                        match = match.replace(argKey, indentLevel > 0 ? '$1 → ' : '$1: ');
+                        match = match.replace('.', '');
+                        style = getStyle('property', indentLevel);
                         break;
                     case '{':
-                        style = getStyle('brace', indentationLevel);
+                        style = getStyle('brace', indentLevel);
                         break;
                     case '}':
                         style = getStyle('brace', closingIndentation);
                         break;
                     case '[':
-                        style = getStyle('bracket', indentationLevel);
+                        style = getStyle('bracket', indentLevel);
                         break;
                     case ']':
                         style = getStyle('bracket', closingIndentation);
                         break;
+                    case '|':
+                        style = getStyle('pipe');
+                        break;
                     default:
-                        style = getStyle('nullValue', indentationLevel);
+                        style = getStyle('nullValue', indentLevel);
                         break;
                 }
                 consoleArgs.push(style, match, '');
-                return replacement; 
+                return replacement;
             }
-            
+        
             function getIndetationLevel(str) {
                 var openingSymbolsCount = (str.match(/\{|\[/gm) || []).length;
                 var closingSymbolsCount = (str.match(/\}|\]/gm) || []).length;
                 return openingSymbolsCount - closingSymbolsCount;
             }
-            
+        
             function breakAndIndentBefore(match, offset, str, indentationStr) {
                 var indentLevel = getIndetationLevel(str.substr(0, offset + 1));
-                var indent = Array(indentLevel).fill(indentationStr).join('');
+                var indent = getIndent(indentLevel, indentationStr);
                 return '\\n' + indent + match;
             }
-            
+        
             function breakAndIndentAfter(match, offset, str, indentationStr) {
                 var indentLevel = getIndetationLevel(str.substr(0, offset + 1));
-                var indent = Array(indentLevel).fill(indentationStr).join('');
+                var indent = getIndent(indentLevel, indentationStr);
                 return match + '\\n' + indent;
             }
-            
-            function preprocessJson(json) {
-                json = json.length === 1 ? json[0] : json;
-            
-                if (typeof json == 'object') {
-                    Object.keys(json).forEach(function(key) {
-                        if (/__ARG__/i.test(key)) {
-                            var numKey = parseInt(key.replace(/__ARG__/i, ''));
-                            if (numKey !== undefined) {
-                                json[numKey] = json[key];
-                                delete json[key];
-                            }
-                        }
-                    })
-                }
+        
+            function getIndent(level, str) {
+                return Array(level).fill(str).join('');
             }
-            
-            preprocessJson(jsonDump);
-            
-            var jsonString = JSON.stringify(jsonDump)
-                    .replace(regexes.breakAfter, function(match, offset, str) { return breakAndIndentAfter(match, offset, str, indentationStr) })
-                    .replace(regexes.breakBefore, function(match, offset, str) { return breakAndIndentBefore(match, offset, str, indentationStr)})
-                    .replace(regexes.stylable, function(match, offset, str) { return addStyles(match, offset, str, consoleArgs) });
-            
-            consoleArgs.unshift(jsonString)
-            console.group('PHP LOG');
+        
+            var jsonString = JSON.stringify(jsonDump).replace(/^\{|\}$/gm, '')
+                .replace(regexes.breakAfter, function(match, offset, str) { return breakAndIndentAfter(match, offset, str, indentationStr) })
+                .replace(regexes.breakBefore, function(match, offset, str) { return breakAndIndentBefore(match, offset, str, indentationStr)})
+                .replace(regexes.stylable, function(match, offset, str) { return addStyles(match, offset, str, consoleArgs) });
+        
+        
+            consoleArgs.unshift(jsonString);
+            console.group('PHP');
             console.log.apply(this, consoleArgs);
             console.groupEnd();
+        }
         </script>";
+    }
 }
 ?>
